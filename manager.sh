8b7322c2065cde2d8dd3283590c4ebe2499ec86a
Ashley Sanders <ashleymichaelsanders@gmail.com>
2019-10-17 20:00:42 -0500

    Fixed some issues in manager.sh

--- manager.sh
+++ manager.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# Colours
+# Colours for messages
     NORMAL="\033[0;37m"
     RED="\033[1;31m"
     GREEN="\033[1;32m"
     ORANGE="\033[1;33m"
 	
-# Restart message
-    MSG_30="Server will restart in 30 seconds!"
-	
-# Path
+# FiveM master path
     FIVEM_PATH=/FiveM
 
-# Screen
-    SCREEN="fxserver"
+# Servers terminal screen
+    SCREEN="fivem"
+
+# Restart message
+    RESTART_MESSAGE="Server will restart in 30 seconds"
 
 cd $FIVEM_PATH	
 running(){
@@ -30,72 +30,72 @@ case "$1" in
     start)
 	if ( running )
 	then
-	    echo -e "$RED The server [$SCREEN] is already starting!$NORMAL"
+	    echo -e "$RED The server [$SCREEN] is already running $NORMAL"
 	else
-        echo -e "$ORANGE The server [$SCREEN] will start.$NORMAL"
+        echo -e "$ORANGE The server [$SCREEN] will start momentaril $NORMAL"
 		screen -dm -S $SCREEN
 		sleep 2
 		screen -x $SCREEN -X stuff "cd "$FIVEM_PATH"/server-data && bash "$FIVEM_PATH"/run.sh +exec server.cfg"
-		echo -e "$ORANGE Restart the sessions.$NORMAL"
+		echo -e "$ORANGE Restarting the sessions $NORMAL"
 		sleep 20
 		screen -x $SCREEN -X stuff "restart sessionmanager"
-		echo -e "$GREEN Session good to go! $NORMAL"
+		echo -e "$GREEN Sessions are good to go $NORMAL"
 		sleep 5
-		echo -e "$GREEN Server good to go! $NORMAL"
+		echo -e "$GREEN Server is good to go $NORMAL"
 	fi
     ;;
     # -----------------[ Stop ]------------------ #
     stop)
 	if ( running )
 	then
-		echo -e "$GREEN Shutting server down in 10 seconds. $NORMAL"
-        screen -S $SCREEN -p 0 -X stuff "`printf "say $MSG_30\r"`"; sleep 30
+		echo -e "$GREEN Shutting server down in 10 seconds $NORMAL"
+        screen -S $SCREEN -p 0 -X stuff "`printf "say $RESTART_MESSAGE\r"`"; sleep 10
 		screen -S $SCREEN -X quit
-        echo -e "$RED The server [$SCREEN] is stopping.$NORMAL"
+        echo -e "$RED The server [$SCREEN] is shutting down $NORMAL"
 		sleep 5
-		echo -e "$GREEN Server [$SCREEN] shut down. $NORMAL"
+		echo -e "$GREEN Server [$SCREEN] has shut down $NORMAL"
 		rm -R $FIVEM_PATH/server-data/cache/
-		echo -e "$GREEN Clearing the cache. $NORMAL"
+		echo -e "$GREEN Clearing the cache $NORMAL"
 
 	else
-	    echo -e "The server [$SCREEN] failed to start."
+	    echo -e "The server [$SCREEN] couldn't be stopped, most likely because it was never up to begin with"
 	fi
     ;;
     # ----------------[ Restart ]---------------- #
 	restart)
 	if ( running )
 	then
-	    echo -e "$RED The server [$SCREEN] is already running! $NORMAL"
+	    echo -e "$RED The server [$SCREEN] is currently running $NORMAL"
 	else
-	    echo -e "$GREEN The server [$SCREEN] is off. $NORMAL"
+	    echo -e "$GREEN The server [$SCREEN] is off $NORMAL"
 	fi
-	    echo -e "$RED The server will restart soon... $NORMAL"
-		screen -S $SCREEN -p 0 -X stuff "`printf "say $MSG_30\r"`"; sleep 30
+	    echo -e "$RED The server will restart soon $NORMAL"
+		screen -S $SCREEN -p 0 -X stuff "`printf "say $RESTART_MESSAGE\r"`"; sleep 30
 		screen -S $SCREEN -X quit
 		echo -e "$GREEN Server now off $NORMAL"
 		rm -R $FIVEM_PATH/server-data/cache/
-		echo -e "$GREEN Clearing the cache. $NORMAL"
+		echo -e "$GREEN Clearing the cache $NORMAL"
 		sleep 2
-		echo -e "$ORANGE Server restart in progress ... $NORMAL"
+		echo -e "$ORANGE Server restart in progress $NORMAL"
 		sleep 10
-        echo -e "$ORANGE Server [$SCREEN] about to start.$NORMAL"
+        echo -e "$ORANGE Server [$SCREEN] about to start $NORMAL"
 		screen -dm -S $SCREEN
 		sleep 2
 		screen -x $SCREEN -X stuff "cd "$FIVEM_PATH"/server-data && bash "$FIVEM_PATH"/run.sh +exec server.cfg 
 		"
-		echo -e "$ORANGE Restarting the sessions.$NORMAL"
+		echo -e "$ORANGE Restarting the sessions $NORMAL"
 		sleep 20
 		screen -x $SCREEN -X stuff "restart sessionmanager
 		"
-		echo -e "$GREEN Server [$SCREEN] started! $NORMAL"
+		echo -e "$GREEN Server [$SCREEN] is up and running $NORMAL"
 	;;	
     # -----------------[ Status ]---------------- #
 	status)
 	if ( running )
 	then
-	    echo -e "$GREEN [$SCREEN] running. $NORMAL"
+	    echo -e "$GREEN [$SCREEN] is all systems go $NORMAL"
 	else
-	    echo -e "$RED [$SCREEN] off. $NORMAL"
+	    echo -e "$RED [$SCREEN] is currently offline $NORMAL"
 	fi
 	;;
     # -----------------[ Screen ]---------------- #
@@ -104,7 +104,7 @@ case "$1" in
         screen -R $SCREEN
     ;;
 	*)
-    echo -e "$ORANGE Utilisation:$NORMAL ./manage.sh {start|stop|status|screen|restart}"
+    echo -e "$ORANGE Utilisation:$NORMAL manage.sh {start|stop|status|screen|restart}"
     exit 1
     ;;
 esac
