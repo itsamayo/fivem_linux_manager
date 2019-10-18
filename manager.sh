ce8bcb2776df8b63c0569c23d0fb7d7e7cb21338
Ashley Sanders <ashleymichaelsanders@gmail.com>
2019-10-17 20:31:33 -0500

    Fixed some issues in manager.sh

--- manager.sh
+++ manager.sh
@@ -3,11 +3,11 @@
 # FiveM master path
     FIVEM_PATH=/FiveM
 
-# Servers terminal screen
+# Servers terminal screen/server name
     SCREEN="fivem"
 
 # Restart message
-    RESTART_MESSAGE="Server will restart in 30 seconds"
+    RESTART_MESSAGE="Server will restart in 15 seconds"
 
 # Colours for messages
     NORMAL="\033[0;37m"
@@ -30,9 +30,9 @@ case "$1" in
     start)
 	if ( running )
 	then
-	    echo -e "$RED The server [$SCREEN] is already running $NORMAL"
+	    echo -e "$RED The [$SCREEN] server is already running $NORMAL"
 	else
-        echo -e "$ORANGE The server [$SCREEN] will start momentarily $NORMAL"
+        echo -e "$ORANGE The [$SCREEN] server will start momentarily $NORMAL"
 		screen -dm -S $SCREEN
 		sleep 2
 		screen -x $SCREEN -X stuff "cd "$FIVEM_PATH"/server-data && bash "$FIVEM_PATH"/run.sh +exec server.cfg"
@@ -41,7 +41,7 @@ case "$1" in
 		screen -x $SCREEN -X stuff "restart sessionmanager"
 		echo -e "$GREEN Sessions are good to go $NORMAL"
 		sleep 5
-		echo -e "$GREEN Server is good to go $NORMAL"
+		echo -e "$GREEN Server is up and running $NORMAL"
 	fi
     ;;
     # -----------------[ Stop ]------------------ #
@@ -49,28 +49,28 @@ case "$1" in
 	if ( running )
 	then
 		echo -e "$GREEN Shutting server down in 10 seconds $NORMAL"
-        screen -S $SCREEN -p 0 -X stuff "`printf "say $RESTART_MESSAGE\r"`"; sleep 10
+        screen -S $SCREEN -p 0 -X stuff "`printf "say $RESTART_MESSAGE\r"`"; sleep 15
 		screen -S $SCREEN -X quit
-        echo -e "$RED The server [$SCREEN] is shutting down $NORMAL"
+        echo -e "$RED The server is shutting down $NORMAL"
 		sleep 5
-		echo -e "$GREEN Server [$SCREEN] has shut down $NORMAL"
+		echo -e "$GREEN Server has shut down $NORMAL"
 		rm -R $FIVEM_PATH/server-data/cache/
 		echo -e "$GREEN Clearing the cache $NORMAL"
 
 	else
-	    echo -e "The server [$SCREEN] couldn't be stopped, most likely because it was never up to begin with"
+	    echo -e "$RED The server couldn't be stopped, most likely because it isn't running $NORMAL"
 	fi
     ;;
     # ----------------[ Restart ]---------------- #
 	restart)
 	if ( running )
 	then
-	    echo -e "$RED The server [$SCREEN] is currently running $NORMAL"
+	    echo -e "$RED The server is currently running $NORMAL"
 	else
-	    echo -e "$GREEN The server [$SCREEN] is off $NORMAL"
+	    echo -e "$GREEN The server is off $NORMAL"
 	fi
 	    echo -e "$RED The server will restart soon $NORMAL"
-		screen -S $SCREEN -p 0 -X stuff "`printf "say $RESTART_MESSAGE\r"`"; sleep 30
+		screen -S $SCREEN -p 0 -X stuff "`printf "say $RESTART_MESSAGE\r"`"; sleep 15
 		screen -S $SCREEN -X quit
 		echo -e "$GREEN Server now off $NORMAL"
 		rm -R $FIVEM_PATH/server-data/cache/
@@ -78,16 +78,16 @@ case "$1" in
 		sleep 2
 		echo -e "$ORANGE Server restart in progress $NORMAL"
 		sleep 10
-        echo -e "$ORANGE Server [$SCREEN] about to start $NORMAL"
+        echo -e "$ORANGE Server about to start $NORMAL"
 		screen -dm -S $SCREEN
 		sleep 2
 		screen -x $SCREEN -X stuff "cd "$FIVEM_PATH"/server-data && bash "$FIVEM_PATH"/run.sh +exec server.cfg 
 		"
-		echo -e "$ORANGE Restarting the sessions $NORMAL"
+		echo -e "$ORANGE Restarting all sessions $NORMAL"
 		sleep 20
 		screen -x $SCREEN -X stuff "restart sessionmanager
 		"
-		echo -e "$GREEN Server [$SCREEN] is up and running $NORMAL"
+		echo -e "$GREEN [$SCREEN] is up and running $NORMAL"
 	;;	
     # -----------------[ Status ]---------------- #
 	status)
@@ -100,11 +100,11 @@ case "$1" in
 	;;
     # -----------------[ Screen ]---------------- #
     screen)
-        echo -e "$GREEN Server screen [$SCREEN]. $NORMAL"
+        echo -e "$GREEN Server screen is [$SCREEN]. $NORMAL"
         screen -R $SCREEN
     ;;
 	*)
-    echo -e "$ORANGE Utilisation:$NORMAL manage.sh {start|stop|status|screen|restart}"
+    echo -e "$ORANGE Utilisation:$NORMAL manager.sh {start|stop|status|screen|restart}"
     exit 1
     ;;
 esac
